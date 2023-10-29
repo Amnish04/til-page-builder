f07bffeec72c1db7220bf44a6e1f89485472a690
Amnish <amnishsingh04@gmail.com>
2023-10-28 23:27:48 -0400

    Finish prototyping TOC generation

---
+++ src/builder/toc_generator/heading_item.py
@@ -0,0 +1,38 @@
+from typing import List
+import hashlib
+
+class HeadingItem:
+    def __init__(self, value: str, children: List['HeadingItem'] = []):
+        """Constructor
+
+        :param id: id of the corresponding html element
+        :param value: inner html of the corresponding html element
+        :param children: child nodes
+        """
+        self.id = HeadingItem.generate_heading_id(value)
+        self.value = value
+        self.children = children
+
+    @staticmethod
+    def generate_heading_id(text_content: str):
+        """Generate a unique hash id based on the heading text
+
+        :param text_content: Text content of the heading
+        :return: A unique `sha512` id for the heading
+        """
+        # Create a SHA-512 hash object
+        sha512_hash = hashlib.sha512()
+
+        # Update the hash object with the bytes of the string
+        sha512_hash.update(text_content.encode())
+
+        # Get the hexadecimal representation of the hash
+        return sha512_hash.hexdigest()
+    
+    def get_html(self) -> str:
+        return f"""
+            <li>
+                <a href='#{self.id}'>{self.value}</a>
+            </li>
+            """
+    
